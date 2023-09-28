5437c8d55a0300339cf45928be5418967af3ec19
AMillsy <00adammills00@gmail.com>
2023-09-28 19:51:06 +0100

    Creating a text field using material ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,17 +2,44 @@ import { Link } from "react-router-dom";
 import "../styles/Home.css";
 import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
+import AService from "../utils/Avatar";
+import { Avatar, TextField } from "@mui/material";
+
 const Home = () => {
   return (
-    <Stack spacing={1} alignItems="stretch" style={{ width: "100%" }}>
+    <div className="main">
       {/* For variant="text", adjust the height via font-size */}
-      <div className="userInfo">Top Bar</div>
+      <div className="userInfo">
+        <Avatar
+          alt="User icon"
+          sx={{
+            width: 45,
+            height: 45,
+            bgcolor: AService.stringToColor("Jake"),
+          }}
+          className="userIcon"
+        >
+          {AService.stringAvatar("Jake Bummer")}
+        </Avatar>
+        <p>Jake Bummer</p>
+      </div>
 
-      {/* For other variants, adjust the size with `width` and `height` */}
-      <Skeleton variant="circular" width={40} height={40} />
-      <Skeleton variant="rectangular" width={210} height={60} />
-      <Skeleton variant="rounded" width={210} height={60} />
-    </Stack>
+      <div className="messenger">
+        {/* For other variants, adjust the size with `width` and `height` */}
+        <div className="textFieldCon">
+          <TextField
+            id="filled-text"
+            label="Messenge"
+            variant="filled"
+            maxRows={3}
+            sx={{ width: "90%", background: "#212121" }}
+            color="secondary"
+            autoComplete="on"
+            multiline
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
