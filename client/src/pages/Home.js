8848d1f15d087ef4650b3d655a51116b26061a05
AMillsy <00adammills00@gmail.com>
2023-09-28 19:15:21 +0100

    setting up the main messager and laid out section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 const Home = () => {
   return (
-    <article className="home-main">
-      <aside className="user-section">Hi</aside>
-      <div className="mainMessageSection">Hey</div>
-    </article>
+    <Stack spacing={1} alignItems="stretch" style={{ width: "100%" }}>
+      {/* For variant="text", adjust the height via font-size */}
+      <div className="userInfo">Top Bar</div>
+
+      {/* For other variants, adjust the size with `width` and `height` */}
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={60} />
+      <Skeleton variant="rounded" width={210} height={60} />
+    </Stack>
   );
 };
 
--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,10 +1,112 @@
-import { Outlet } from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import { Avatar } from "@mui/material";
+import AService from "../utils/Avatar";
+import "../styles/Layout.css";
 const Layout = () => {
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      backgroundColor: "#44b700",
+      color: "#44b700",
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      "&::after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "50%",
+        animation: "ripple 1.2s infinite ease-in-out",
+        border: "1px solid currentColor",
+        content: '""',
+      },
+    },
+    "@keyframes ripple": {
+      "0%": {
+        transform: "scale(.8)",
+        opacity: 1,
+      },
+      "100%": {
+        transform: "scale(2.4)",
+        opacity: 0,
+      },
+    },
+  }));
+
   return (
     <>
-      <h2>Layout</h2>
-      <Outlet />
+      <article className="home-main">
+        <aside className="user-section">
+          <div className="userIcon">
+            <Link>
+              {/* <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            > */}
+              <Avatar
+                alt="User icon"
+                sx={{
+                  width: 80,
+                  height: 80,
+                  bgcolor: AService.stringToColor("Dave"),
+                }}
+                className="userIcon"
+              >
+                {AService.stringAvatar("Dave Jones")}
+              </Avatar>
+              {/* </StyledBadge> */}
+            </Link>
+          </div>
+          <div className="friendList">
+            <ul>
+              <li className="friend">
+                <Avatar
+                  alt="User icon"
+                  sx={{
+                    width: 60,
+                    height: 60,
+                    bgcolor: AService.stringToColor("Jake"),
+                  }}
+                  className="userIcon"
+                >
+                  {AService.stringAvatar("Jake Bummer")}
+                </Avatar>
+              </li>
+              <li className="friend">
+                <Avatar
+                  alt="User icon"
+                  sx={{
+                    width: 60,
+                    height: 60,
+                    bgcolor: AService.stringToColor("Jake"),
+                  }}
+                  className="userIcon"
+                >
+                  {AService.stringAvatar("Jake Bummer")}
+                </Avatar>
+              </li>
+              <li className="friend">
+                <Avatar
+                  alt="User icon"
+                  sx={{
+                    width: 60,
+                    height: 60,
+                    bgcolor: AService.stringToColor("Jake"),
+                  }}
+                  className="userIcon"
+                >
+                  {AService.stringAvatar("Jake Bummer")}
+                </Avatar>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        <div className="mainMessageSection">
+          <Outlet />
+        </div>
+      </article>
     </>
   );
 };
