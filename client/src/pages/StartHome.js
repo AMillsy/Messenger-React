85a9a01d80162ce34d66b6db00932f88325f10d3
AMillsy <00adammills00@gmail.com>
2023-10-17 12:19:27 +0100

    revamping the homepage

--- client/src/pages/StartHome.js
+++ client/src/pages/StartHome.js
@@ -5,41 +5,20 @@ import HomeShowcase from "../components/HomeShowcase";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import MessageIcon from "@mui/icons-material/Message";
 import Groups2Icon from "@mui/icons-material/Groups2";
+import wave from "../images/wave-haikei.svg";
 const StartHome = () => {
   return (
     <div className="startMain">
-      <div className="startTop">
-        <h2>Come to a world</h2>
-        <h3>Where messaging is so easy</h3>
-        <h3>And finding and adding friends is even easier</h3>
-      </div>
-      <div className="signInSection">
-        <h3>Create your own Journey</h3>
-        <div className="startLinks">
-          <Link className="linkBtn startLogin" to={"/login"}>
-            Login to an Account
-          </Link>
-          <Link className="linkBtn startSignup" to={"/signup"}>
-            Create a new Account
-          </Link>
-        </div>
-      </div>
-      <div className="startShowcase">
-        <HomeShowcase
-          svgIcon={<GroupAddIcon className="svgIcon" />}
-          text={"Add friends..."}
-          num={1}
-        />
-        <HomeShowcase
-          svgIcon={<MessageIcon className="svgIcon" />}
-          text={"Message to your hearts content..."}
-          num={2}
-        />
-        <HomeShowcase
-          svgIcon={<Groups2Icon className="svgIcon" />}
-          text={"Create groups!"}
-          num={3}
-        />
+      <section className="wave-con">
+        <h1 className="homeTitle">TalkTide</h1>
+        <p>Message easily and create groups to find new people</p>
+        <p>Create or find place to belong</p>
+
+        <img src={wave} className="waveHomeImage"></img>
+      </section>
+      <div className="homeLinks">
+        <Link className="linkBtn">Login</Link>
+        <Link className="linkBtn signupBtn">Signup</Link>
       </div>
     </div>
   );
